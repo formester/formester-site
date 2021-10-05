f75c9a4feb3e85059b4ca18e8247a62ff7e80aed
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2021-10-05 19:46:08 +0530

    made seperate components for home

---
+++ components/home/OtherSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Section
+      heading="Powerful Integration"
+      content="Connect your form with Slack, Google Sheets, Salesforce or anything else with our Zapier integration. Use our webhooks to notify any other app when a submission arrives."
+    />
+    <Section
+      heading="Pre-designed Templates"
+      content="Save your time and energy with templates that are already designed according to need common needs of the user. Formester comes with a lot of templates to cover common use cases."
+      alter="true"
+    />
+  </div>
+</template>
+
+<script>
+import Section from '@/components/Section.vue'
+
+export default {
+  components: {
+    Section,
+  },
+}
+</script>
