1665041e159c9b6628dfe628a10330ba34552938
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2021-10-05 15:44:53 +0530

    conflict resolved

--- pages/index.vue
+++ pages/index.vue
@@ -25,11 +25,7 @@ export default {
       heading: 'Easy Web forms',
       content:
         'Creating a form has never been simpler. Just point your form to our endpoint and start getting submissions in a matter of minutes. If you are a power user you get complete control over how the form looks.',
-<<<<<<< HEAD
       // image: 'integration.png',
-=======
-      image: 'integration.png',
->>>>>>> a0efaa25e66ebeff56389521506a6b2abe438801
     }
   },
 }
