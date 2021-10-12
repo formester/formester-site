ab399564d8de04f21fe2f25e129b455c52b6685a
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2021-10-12 10:53:29 +0530

    testimonials section changes

--- components/home/Testimonials.vue
+++ components/home/Testimonials.vue
@@ -1,5 +1,5 @@
 <template>
-  <section
+  <!-- <section
     class="
       testimonials
       position-relative
@@ -25,6 +25,40 @@
       alt="NA"
       class="position-absolute testimonial__svg"
     />
+  </section> -->
+   <section class="container my-5">
+    <div class="row gx-5">
+      <div
+        class="
+          col-md-3
+          d-flex
+          flex-column
+          justify-content-center
+          align-items-md-start align-items-center
+          text-center text-md-start
+          ms-md-5
+        "
+      >
+        <h3 class="section__heading">Testimonials</h3>
+        <p class="mt-3">
+          Here’s What Some of Our Happy Customer Have to Say About Us!
+        </p>
+      </div>
+      <div class="col-md-8 d-flex justify-content-center mt-md-0 mt-5 position-relative overflow-hidden">
+       <div class="testimonials d-flex flex-column">
+         <p>I really enjoy Formester. It’s clean and straightforward and it does well it is supposed to do. </p>
+         <span class="testimonial__person-name">
+            Thibaud Martinez
+         </span>
+         <span class="testimonial__person-post">
+            Organizer
+         </span>
+         <img src="../../assets/images/tedx.png" alt="Brand-Logo" height="40" width="110">
+       </div>
+       <img src="../../assets/images/testimonial.svg" alt="NA" class="testimonial-bg">
+       <img src="../../assets/images/quotes.svg" alt="NA" class="quotes">
+      </div>
+    </div>
   </section>
 </template>
 
@@ -34,16 +68,15 @@ export default {}
 
 <style scoped>
 .testimonials {
-  height: 600px;
+  padding: 7em 0;
   color: var(--clr-primary-light);
-  margin-bottom: 5em;
-  overflow: hidden;
 }
 
-.testimonial__svg {
-  top: -15%;
-  left: -45%;
-  z-index: -1;
+.testimonial-bg {
+  position: absolute;
+  top: -85%;
+  right: -50%;
+  z-index: -100;
 }
 
 .testimonials__heading {
@@ -70,5 +103,12 @@ export default {}
   letter-spacing: 1px;
   text-transform: uppercase;
   margin-top: -0.4em;
+}
+
+.quotes {
+  position: absolute;
+  top: 10%;
+  right: -10%;
+  z-index: -1;
 }
 </style>
