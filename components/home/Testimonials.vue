e046f9019cdb5fa96b6fd05d3fafaf465bc1bd17
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2021-10-12 11:14:30 +0530

    testimonial styles fix

--- components/home/Testimonials.vue
+++ components/home/Testimonials.vue
@@ -37,6 +37,7 @@
           align-items-md-start align-items-center
           text-center text-md-start
           ms-md-5
+          light-bg
         "
       >
         <h3 class="section__heading">Testimonials</h3>
@@ -46,14 +47,14 @@
       </div>
       <div class="col-md-8 d-flex justify-content-center mt-md-0 mt-5 position-relative overflow-hidden">
        <div class="testimonials d-flex flex-column">
-         <p>I really enjoy Formester. It’s clean and straightforward and it does well it is supposed to do. </p>
-         <span class="testimonial__person-name">
+         <p class="testimonial__content">I really enjoy Formester. It’s clean and straightforward and it does well it is supposed to do. </p>
+         <span class="testimonial__person--name">
             Thibaud Martinez
          </span>
-         <span class="testimonial__person-post">
+         <span class="testimonial__person--post">
             Organizer
          </span>
-         <img src="../../assets/images/tedx.png" alt="Brand-Logo" height="40" width="110">
+         <img src="../../assets/images/tedx.png" alt="Brand-Logo" height="40" width="110" class="mt-3">
        </div>
        <img src="../../assets/images/testimonial.svg" alt="NA" class="testimonial-bg">
        <img src="../../assets/images/quotes.svg" alt="NA" class="quotes">
@@ -68,13 +69,13 @@ export default {}
 
 <style scoped>
 .testimonials {
-  padding: 7em 0;
-  color: var(--clr-primary-light);
+  padding: 7em;
+  color: white;
 }
 
 .testimonial-bg {
   position: absolute;
-  top: -85%;
+  top: -65%;
   right: -50%;
   z-index: -100;
 }
@@ -85,24 +86,20 @@ export default {}
 
 .testimonial__content {
   font-size: var(--ft-bigger-body);
-  line-height: 40px;
-  max-width: 750px;
-  margin: 1.6em 1.5em 0;
-}
-
-.testimonial__person {
-  margin-top: 1.6em;
+  line-height: 48px;
 }
 
 .testimonial__person--name {
   font-size: var(--ft-big-body);
+  font-weight: 900;
+  margin-top: 1.6em;
 }
+
 
 .testimonial__person--post {
   font-size: var(--ft-df-body);
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  margin-top: -0.4em;
+  letter-spacing: 2%;
+  color: var(--clr-primary-light);
 }
 
 .quotes {
