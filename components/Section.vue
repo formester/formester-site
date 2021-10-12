e109a407d446eaac8e6fd1f848f08a2b8d21bc81
Piyush Singhania <piyushsinghania28@gmail.com>
2021-10-12 11:40:01 +0530

    Refactored the Feature and Section Component, made it more reusable

--- components/Section.vue
+++ components/Section.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container my-5">
-    <div class="row gx-5">
+    <div class="row gx-5" v-if="feature">
       <div
         class="
           col-md-5
@@ -11,17 +11,17 @@
           text-center text-md-start
           ms-md-5
         "
-        :class="{ 'order-md-last': !alter }"
+        :class="{ 'order-md-last': !feature.alter }"
       >
-        <h3 class="section__heading">{{ heading }}</h3>
+        <h3 class="section__heading">{{ feature.heading }}</h3>
         <p class="mt-3">
-          {{ content }}
+          {{ feature.content }}
         </p>
         <a href="" class="feature__button">Learn more</a>
       </div>
       <div class="col-md-6 d-flex justify-content-center mt-md-0 mt-5">
         <img
-          src="require(`../assets/images/${imgName}`)"
+          :src="require(`../assets/images/${feature.imgName}`)"
           alt="Hero-Image"
           class="img-fluid"
         />
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  props: ['heading', 'content', 'imgName', 'alter'],
+  props: ['feature'],
 }
 </script>
 
--- components/home/Features.vue
+++ components/home/Features.vue
@@ -1,29 +1,8 @@
 <template>
   <div>
-    <Section
-      heading="Easy Web forms"
-      content="Creating a form has never been simpler. Just point your form to our endpoint and start getting submissions in a matter of minutes. If you are a power user you get complete control over how the form looks."
-      imgName="auto-response.png"
-    />
-    <Section
-      heading="Auto Responser"
-      content="Respond to the customer as soon as you get a submission. The email is easy to create with our intuitive editor. Sending customised HTML email is also an option for the power user."
-      alter="true"
-      imgName="auto-response.png"
-    />
-    <Section
-      heading="Spam Protection"
-      content="The web form is an easy target for spam. Use our one-click Recaptcha to curb spam and keep your list clean. You can customise this to meet the look and feel of your app."
-      imgName="auto-response.png"
-
-    />
-    <Section
-      heading="Powerful Analytics"
-      content="Understand how users are interacting with your form. Get insights on ways to improve engagement on your form. Identify trends to see when you get most responses."
-      alter="true"
-      imgName="auto-response.png"
-
-    />
+    <div v-for="feature in features" :key="feature.id">
+      <Section :feature="feature" />
+    </div>
   </div>
 </template>
 
@@ -33,6 +12,44 @@ import Section from '@/components/Section.vue'
 export default {
   components: {
     Section,
+  },
+  data() {
+    return {
+      features: [
+        {
+          id: 0,
+          heading: 'Easy Web forms',
+          content:
+            'Creating a form has never been simpler. Just point your form to our endpoint and start getting submissions in a matter of minutes. If you are a power user you get complete control over how the form looks.',
+          alter: false,
+          imgName: 'auto-response.png',
+        },
+        {
+          id: 1,
+          heading: 'Auto Responser',
+          content:
+            'Respond to the customer as soon as you get a submission. The email is easy to create with our intuitive editor. Sending customised HTML email is also an option for the power user.',
+          alter: true,
+          imgName: 'auto-response.png',
+        },
+        {
+          id: 2,
+          heading: 'Spam Protection',
+          content:
+            'The web form is an easy target for spam. Use our one-click Recaptcha to curb spam and keep your list clean. You can customise this to meet the look and feel of your app.',
+          alter: false,
+          imgName: 'auto-response.png',
+        },
+        {
+          id: 3,
+          heading: 'Powerful Analytics',
+          content:
+            'Understand how users are interacting with your form. Get insights on ways to improve engagement on your form. Identify trends to see when you get most responses.',
+          alter: true,
+          imgName: 'auto-response.png',
+        },
+      ],
+    }
   },
 }
 </script>
