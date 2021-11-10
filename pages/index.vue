5bd3d31e0fa277357fa82a5a341e964c0f70c54b
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-11-10 23:27:36 +0530

    added call to action section in homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <Hero />
-    <HowItWorks />
-    <Features />
+    <!-- <Features /> -->
     <Testimonials />
+    <HowItWorks />
     <OtherSection />
     <CallToActionSection />
   </div>
