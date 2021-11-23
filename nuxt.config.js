4fa92f7ce242dec9751184600cb8f4c825671b20
Ankit Singhaniya <ankit.singhaniyaz@gmail.com>
2021-11-24 00:21:56 +0530

    attempt to fix blocking fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap' }
+      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap' }
     ],
     // Custom Javascript
     script: [
