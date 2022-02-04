0d5f9ad1df71265ea02bf9db137a2d6c58b41724
Piyush Singhania <piyushsinghania28@gmail.com>
2022-02-04 20:38:04 +0530

    Created the thank-you page

---
+++ pages/thank-you.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="message-block">
+    <h1 class="message">Thank You</h1>
+    <h4 class="message-detail">for choosing Formester</h4>
+    <button class="navigate-btn" @click="$router.push('/')">Home</button>
+    <img class="abs corner-design" src="../assets/images/leaf/corner-design.png" alt="">
+    <img class="abs l1" src="../assets/images/leaf/leaf-1.png" alt="">
+    <img class="abs l2" src="../assets/images/leaf/leaf-2.png" alt="">
+    <img class="abs l3" src="../assets/images/leaf/leaf-3.png" alt="">
+    <img class="abs l4" src="../assets/images/leaf/leaf-4.png" alt="">
+    <img class="abs l5" src="../assets/images/leaf/leaf-5.png" alt="">
+    <img class="abs l6" src="../assets/images/leaf/leaf-6.png" alt="">
+    <img class="abs l7" src="../assets/images/leaf/leaf-7.png" alt="">
+    <img class="abs l8" src="../assets/images/leaf/leaf-8.png" alt="">
+    <img class="abs l9" src="../assets/images/leaf/leaf-9.png" alt="">
+    <img class="abs l10" src="../assets/images/leaf/leaf-10.png" alt="">
+    <img class="abs l11" src="../assets/images/leaf/leaf-11.png" alt="">
+    <img class="abs l12" src="../assets/images/leaf/leaf-12.png" alt="">
+    <img class="abs l13" src="../assets/images/leaf/leaf-13.png" alt="">
+    <img class="abs l14" src="../assets/images/leaf/leaf-14.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'message'
+}
+</script>
+
+<style>
+.message {
+  font-size: 96px;
+  line-height: 128px;
+}
+.l1 {
+  right: 200px;
+  bottom: 100px;
+}
+.l2 {
+  left: 682px;
+  bottom: 120px;
+}
+.l3 {
+  left: 348px;
+  bottom: 140px;
+}
+.l4 {
+  right: 150px;
+  top: 350px;
+}
+.l5 {
+  left: 509px;
+  top: 200px;
+}
+.l6 {
+  right: 750px;
+  top: 100px;
+}
+.l7 {
+  top: 120px;
+  right: 150px;
+}
+.l8 {
+  left: 576px;
+  top: 60px;
+}
+.l9 {
+  left: 416px;
+  top: 170px;
+}
+.l10 {
+  right: 400px;
+  top: 100px;
+}
+.l11 {
+  left: 252px;
+  top: 400px;
+}
+.l12 {
+  right: 250px;
+  top: 200px;
+}
+.l13 {
+  left: 344.39px;
+  top: 120px;
+}
+.l14 {
+  left: 231px;
+  top: 210px;
+}
+</style>
