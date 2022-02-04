37ce6161cf8cd484e801f025fdadb0c771a7bec3
Piyush Singhania <piyushsinghania28@gmail.com>
2022-02-04 22:07:59 +0530

    Made the thank-you page responsive

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="message-block">
-    <h1 class="message">Thank You</h1>
+    <h1 class="message-thank-you">Thank You</h1>
     <h4 class="message-detail">for choosing Formester</h4>
     <button class="navigate-btn" @click="$router.push('/')">Home</button>
     <img class="abs corner-design" src="../assets/images/leaf/corner-design.png" alt="">
@@ -28,9 +28,14 @@ export default {
 </script>
 
 <style>
-.message {
+.message-thank-you {
+  color: var(--clr-primary);
+  font-family: Fredoka One;
+  font-style: normal;
+  font-weight: normal;
   font-size: 96px;
-  line-height: 128px;
+  line-height: 116px;
+  letter-spacing: 0.04em;
 }
 .l1 {
   right: 200px;
@@ -87,5 +92,110 @@ export default {
 .l14 {
   left: 231px;
   top: 210px;
+}
+@media only screen and (max-width: 1350px) {
+  
+}
+@media only screen and (max-width: 1200px) {
+  
+}
+@media only screen and (max-width: 992px) {
+  .message-thank-you {
+    font-size: 72px;
+    line-height: 88px;
+  }
+  .l2 {
+    left: 400px;
+    bottom: 120px;
+  }
+  .l3 {
+    left: 200px;
+    bottom: 140px;
+  }
+  .l5 {
+    left: 450px;
+  }
+  .l6{
+    display: none;
+  }
+  .l11 {
+    left: 150px;
+    top: 400px;
+  }
+  .l13 {
+    left: 250px;
+    top: 120px;
+  }
+  .l14 {
+    left: 130px;
+    top: 210px;
+  }
+}
+@media only screen and (max-width: 768px) {
+  .message-thank-you {
+    font-size: 48px;
+    line-height: 56px;
+  }
+  .l2 {
+    left: 360px;
+    bottom: 140px;
+  }
+  .l3 {
+    left: 100px;
+  }
+  .l4 {
+    right: 50px;
+    top: 350px;
+  }
+  .l5 {
+    display: none;
+  }
+  .l8 {
+    display: none;
+  }
+  .l10 {
+    right: 320px;
+  }
+  .l11 {
+    left: 80px;
+  }
+  .l13 {
+    left: 180px;
+    top: 80px;
+  }
+  .l14 {
+    left: 60px;
+  }
+}
+@media only screen and (max-width: 576px) {
+  .message-thank-you {
+    font-size: 32px;
+    line-height: 40px;
+  }
+  .l2 {
+    left: 260px;
+  }
+  .l4 {
+    top: 400px;
+    right: 30px;
+  }
+  .l7 {
+    right: 60px;
+  }
+  .l9 {
+    display: none;
+  }
+  .l10 {
+    right: 250px;
+  }
+  .l11 {
+    left: 30px;
+  }
+  .l12 {
+    display: none;
+  }
+  .l13 {
+    top: 40px;
+  }
 }
 </style>
