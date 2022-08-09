00c6632db262fe2c598a8f518e68a358f2583694
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2022-08-09 23:29:44 +0530

    new footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,7 @@
           <img src="../assets/images/logo-light.svg" alt="Formester" />
         </div>
       </NuxtLink>
-      <ul
+      <!-- <ul
         class="footer__items d-flex flex-column flex-lg-row align-items-center justify-content-center mt-4"
       >
         <NuxtLink to="/">
@@ -36,38 +36,138 @@
         <a href="mailto:support@formester.com">
           <li class="mx-3 footer__link">Get Support</li>
         </a>
-      </ul>
-      <div class="socials mt-4">
-        <a
-          href="https://instagram.com/_formester_?utm_medium=copy_link"
-          class="mx-1"
-          target="_blank"
-          ><img
-            src="@/assets/images/social/instagram.svg"
-            alt="Fomester instagram page"
-        /></a>
-        <a
-          href="https://twitter.com/_Formester_?t=-m1pNwXvxR6KOf9kfPi9lQ&s=09"
-          class="mx-1"
-          target="_blank"
-          ><img
-            src="@/assets/images/social/twitter.svg"
-            alt="Formester twitter page"
-        /></a>
-        <a
-          href="https://www.facebook.com/formester"
-          class="mx-1"
-          target="_blank"
-          ><img
-            src="@/assets/images/social/facebook.svg"
-            alt="Fomester facebook page"
-        /></a>
-        <!-- <a href="#" class="mx-1" target="_blank"
-          ><img
-            src="@/assets/images/social/youtube.svg"
-            alt="Formester youtube page"
-        /></a> -->
-      </div>
+      </ul> -->
+      <section class="mt-5 container">
+        <div class="row">
+          <div class="col-lg-3 col-sm-6 mb-4 mb-md-0 text-lg-start">
+            <h5 class="text-uppercase footer__items--heading mb-3">
+              Foremster
+            </h5>
+
+            <ul class="list-unstyled mt-2">
+              <li class="mt-2">
+                <NuxtLink to="/" class="footer__link">Home</NuxtLink>
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/pricing" class="footer__link">Pricing</NuxtLink>
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/blog" class="footer__link">Blog</NuxtLink>
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/integrations" class="footer__link"
+                  >Integrations</NuxtLink
+                >
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-lg-3 col-sm-6 mb-4 mb-md-0 text-lg-start">
+            <h5 class="text-uppercase footer__items--heading mb-3">Feature</h5>
+
+            <ul class="list-unstyled mt-2">
+              <li class="mt-2">
+                <NuxtLink to="/features/html-form-backend" class="footer__link"
+                  >HTML Form Backend</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/features/auto-responder" class="footer__link"
+                  >Auto Responder</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/features/spam-protection" class="footer__link"
+                  >Spam Protection</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/features/powerful-analytics" class="footer__link"
+                  >Powerful Protection</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/features/conditional-logic" class="footer__link"
+                  >Conditional Logic</NuxtLink
+                >
+              </li>
+            </ul>
+          </div>
+
+          <div
+            class="col-lg-3 col-sm-6 mb-4 mb-md-0 mt-0 mt-sm-4 mt-lg-0 text-lg-start"
+          >
+            <h5 class="text-uppercase footer__items--heading mb-3">Others</h5>
+
+            <ul class="list-unstyled mt-2">
+              <li class="mt-2">
+                <NuxtLink to="/privacy" class="footer__link"
+                  >Privacy Policy</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/terms-of-service" class="footer__link"
+                  >Terms of Service</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <NuxtLink to="/contact" class="footer__link"
+                  >Contact Us</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <a href="mailto:support@formester.com" class="footer__link"
+                  >Get Support</a
+                >
+              </li>
+            </ul>
+          </div>
+
+          <div
+            class="col-lg-3 col-sm-6 mb-4 mb-md-0 mt-0 mt-sm-4 mt-lg-0 text-lg-start"
+          >
+            <h5 class="text-uppercase footer__items--heading mb-3">Compare</h5>
+
+            <ul class="list-unstyled mt-2">
+              <li class="mt-2">
+                <NuxtLink to="/formester-vs-typeform" class="footer__link"
+                  >Formester vs Typeform</NuxtLink
+                >
+              </li>
+              <li class="mt-2">
+                <div class="socials mt-4">
+                  <a
+                    href="https://instagram.com/_formester_?utm_medium=copy_link"
+                    class="mx-1"
+                    target="_blank"
+                    ><img
+                      src="@/assets/images/social/instagram.svg"
+                      alt="Fomester instagram page"
+                  /></a>
+                  <a
+                    href="https://twitter.com/_Formester_?t=-m1pNwXvxR6KOf9kfPi9lQ&s=09"
+                    class="mx-1"
+                    target="_blank"
+                    ><img
+                      src="@/assets/images/social/twitter.svg"
+                      alt="Formester twitter page"
+                  /></a>
+                  <a
+                    href="https://www.facebook.com/formester"
+                    class="mx-1"
+                    target="_blank"
+                    ><img
+                      src="@/assets/images/social/facebook.svg"
+                      alt="Fomester facebook page"
+                  /></a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
       <div class="divider mt-4"></div>
       <div class="copyright d-flex justify-content-center mt-3">
         © 2021 Acorn Globus. All rights reserved
@@ -86,7 +186,7 @@ ul {
 }
 
 .footer__container {
-  background-color: var(--clr-primary);
+  background: linear-gradient(180deg, #4f3895 0.01%, #1d1240 100%);
   color: var(--clr-primary-light);
   padding: 2em 0 1em 0;
 }
@@ -98,13 +198,17 @@ ul {
   color: white;
 }
 
-.footer__items {
-  list-style: none;
+.footer__items--heading {
+  font-size: 14px;
+  line-height: 21px;
+  letter-spacing: 0.02em;
+  color: var(--clr-primary-light);
 }
 
 .footer__link {
   font-size: 14px;
-  color: var(--clr-primary-light);
+  line-height: 21px;
+  color: white;
 }
 
 .footer__link:hover {
