5c9df462f3985d072802b8bfa846e696129dc704
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2022-10-07 17:31:40 +0530

    new testimonial added and person photo added

--- components/home/Testimonials.vue
+++ components/home/Testimonials.vue
@@ -29,18 +29,44 @@
             <div class="carousel-indicators">
               <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
               <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active">
                 <div class="d-flex flex-column">
                   <p class="testimonial__content mt-2 mt-lg-0">
+                    Formester is really easy to use and an exceptional alternative for Typeform. We use it for call for papers, lead registrations, feedback and surveys and it cannot be simpler.
+                  </p>
+                  <div class="d-flex testimonial__person">
+                    <div class="d-flex align-items-center">
+                      <img src="~/assets/images/testimonials/jilson.png" alt="Jilson">
+                      <div class="d-flex flex-column align-items-start ms-3">
+                        <span class="testimonial__person--name">Jilson</span>
+                        <span class="testimonial__person--post">Vue Toronto</span>
+                      </div>
+                    </div>
+                    <img
+                      src="~/assets/images/vueconf.svg"
+                      alt="Rumie Logo"
+                      width="150"
+                      class="company__logo"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="d-flex flex-column">
+                  <p class="testimonial__content mt-2 mt-lg-0">
                     I really enjoy Formester. It’s clean and straight-forward and it
                     does well, what it is supposed to do.
                   </p>
-                  <div class="d-flex testimonial__person">
-                    <div class="d-flex flex-column">
-                      <span class="testimonial__person--name"> Thibaud Martinez </span>
-                      <span class="testimonial__person--post"> Organizer </span>
+                  <div class="d-flex testimonial__person mt-4r">
+                    <div class="d-flex align-items-center">
+                      <img src="~/assets/images/testimonials/thibaud.png" alt="Thibaud">
+                      <div class="d-flex flex-column align-items-start ms-3">
+                        <span class="testimonial__person--name">Thibaud Martinez</span>
+                        <span class="testimonial__person--post">Organizer</span>
+                      </div>
                     </div>
                     <img
                       src="~/assets/images/tedx.png"
@@ -56,10 +82,13 @@
                   <p class="testimonial__content mt-2 mt-lg-0">
                     At Rumie we use Formester to get feedback from our educators and volunteers. It's intuitive, fast and easy to share. 
                   </p>
-                  <div class="d-flex testimonial__person">
-                    <div class="d-flex flex-column">
-                      <span class="testimonial__person--name"> Bogdan Arsenie </span>
-                      <span class="testimonial__person--post"> Chief Technology Officer </span>
+                  <div class="d-flex testimonial__person mt-4r">
+                    <div class="d-flex align-items-center">
+                      <img src="~/assets/images/testimonials/bogdan.png" alt="Bogdan">
+                      <div class="d-flex flex-column align-item-start ms-3">
+                        <span class="testimonial__person--name">Bogdan Arsenie</span>
+                        <span class="testimonial__person--post">Chief Technology Officer </span>
+                      </div>
                     </div>
                     <img
                       src="~/assets/images/rumie.svg"
@@ -89,6 +118,9 @@ export default {}
 </script>
 
 <style scoped>
+.mt-4r {
+  margin-top: 4rem !important;
+}
 .product-testimonials {
   display: flex;
   padding-top: 4rem;
