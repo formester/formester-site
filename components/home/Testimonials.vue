148c636e9e6ff6f139aa09c9cfaf9b1ef8b34c6b
Piyush Singhania <piyushsinghania28@gmail.com>
2022-12-29 01:42:33 +0530

    Created the new testimonials section

--- components/home/Testimonials.vue
+++ components/home/Testimonials.vue
@@ -1,271 +1,151 @@
 <template>
-  <section class="testimonials position-relative overflow-hidden">
-    <div class="container">
-      <div class="row gx-5">
+  <div class="container testimonials-section">
+    <div>
+      <h2 class="testimonials-heading">Testimonials</h2>
+      <div class="d-flex flex-column">
         <div
-          class="
-            col-lg-4
-            d-flex
-            align-items-start
-            justify-content-center
-            flex-column
-            testimonials__heading
-            position-relative
-          "
+          v-for="testimonial in testimonials"
+          :key="testimonial.id"
+          class="user d-flex align-items-center"
+          :class="{ active: active === testimonial.id }"
+          @click="active = testimonial.id"
         >
-          <h2 class="section__heading">Testimonials</h2>
-          <p class="mt-lg-3 section__subtitle">
-            Here’s what some of our happy customer have to say about us!
-          </p>
           <img
-            src="~/assets/images/home/testimonials-heading-bg.png"
-            alt="Testimonial background vector image"
-            class="position-absolute testimonials-heading-bg"
+            :src="
+              require(`~/assets/images/testimonials/${testimonial.picture}`)
+            "
+            :alt="testimonial.picture"
           />
-        </div>     
-
-        <div class="col-lg-7 position-relative product-testimonials">
-          <div id="carouselTestimonial" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <div class="d-flex flex-column">
-                  <p class="testimonial__content mt-2 mt-lg-0">
-                    Formester is really easy to use and an exceptional alternative for Typeform. We use it for call for papers, lead registrations, feedback and surveys and it cannot be simpler.
-                  </p>
-                  <div class="d-flex testimonial__person">
-                    <div class="d-flex align-items-center">
-                      <img src="~/assets/images/testimonials/jilson.png" alt="Jilson">
-                      <div class="d-flex flex-column align-items-start ms-3">
-                        <span class="testimonial__person--name">Jilson</span>
-                        <span class="testimonial__person--post">Organiser</span>
-                      </div>
-                    </div>
-                    <img
-                      src="~/assets/images/vueconf.svg"
-                      alt="Rumie Logo"
-                      class="company__logo"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div class="d-flex flex-column">
-                  <p class="testimonial__content mt-2 mt-lg-0">
-                    I really enjoy Formester. It’s clean and straight-forward and it
-                    does well, what it is supposed to do.
-                  </p>
-                  <div class="d-flex testimonial__person mt-4r">
-                    <div class="d-flex align-items-center">
-                      <img src="~/assets/images/testimonials/thibaud.png" alt="Thibaud">
-                      <div class="d-flex flex-column align-items-start ms-3">
-                        <span class="testimonial__person--name">Thibaud Martinez</span>
-                        <span class="testimonial__person--post">Organizer</span>
-                      </div>
-                    </div>
-                    <img
-                      src="~/assets/images/tedx.png"
-                      alt="Tedx Paris Organizer"
-                      width="110"
-                      class="company__logo"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div class="d-flex flex-column">
-                  <p class="testimonial__content mt-2 mt-lg-0">
-                    At Rumie we use Formester to get feedback from our educators and volunteers. It's intuitive, fast and easy to share. 
-                  </p>
-                  <div class="d-flex testimonial__person mt-4r">
-                    <div class="d-flex align-items-center">
-                      <img src="~/assets/images/testimonials/bogdan.png" alt="Bogdan">
-                      <div class="d-flex flex-column align-item-start ms-3">
-                        <span class="testimonial__person--name">Bogdan Arsenie</span>
-                        <span class="testimonial__person--post">Chief Technology Officer </span>
-                      </div>
-                    </div>
-                    <img
-                      src="~/assets/images/rumie.svg"
-                      alt="Rumie Logo"
-                      width="150"
-                      class="company__logo"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <img
-              src="~/assets/images/quotes.svg"
-              alt="Quotes background image"
-              class="quotes"
-            />
+          <div>
+            <h6 class="name">{{ testimonial.user }}</h6>
+            <span class="designation">{{ testimonial.designation }}</span>
           </div>
         </div>
-    
       </div>
     </div>
-  </section>
+    <div class="testimonial-content-wrapper">
+      <p class="mb-5">{{ activeTestimonial.content }}</p>
+      <div class="d-flex justify-content-end">
+        <img
+          :src="
+            require(`~/assets/images/testimonials/${activeTestimonial.logo}`)
+          "
+          :alt="activeTestimonial.logoAlt"
+        />
+      </div>
+    </div>
+    <img
+      src="~/assets/images/testimonials/quote.svg"
+      class="quote-illustraion"
+      alt="Quote illustration"
+    />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      active: 1,
+      testimonials: [
+        {
+          id: 1,
+          user: 'Thibaud Martinez',
+          designation: 'Organiser',
+          picture: 'thibaud.png',
+          content: `I really enjoy Formester. It’s clean and straight-forward and it
+                        does well, what it is supposed to do.`,
+          logo: 'tedx.png',
+          logoAlt: 'Tedx logo',
+        },
+        {
+          id: 2,
+          user: 'Jilson',
+          designation: 'Organiser',
+          picture: 'jilson.png',
+          content: `Formester is really easy to use and an exceptional alternative for Typeform. We use it for call for papers, lead registrations, feedback and surveys and it cannot be simpler.`,
+          logo: 'vueconf.svg',
+          logoAlt: 'Vue conference logo',
+        },
+        {
+          id: 3,
+          user: 'Bogdan Arsenie',
+          designation: 'Chief Technology Officer',
+          picture: 'bogdan.png',
+          content: `At Rumie we use Formester to get feedback from our educators and
+          volunteers. It's intuitive, fast and easy to share.`,
+          logo: 'rumie.svg',
+          logoAlt: 'Rumie logo',
+        },
+      ],
+    }
+  },
+  computed: {
+    activeTestimonial() {
+      return this.testimonials.find(
+        (testimonial) => testimonial.id === this.active
+      )
+    },
+  },
+}
 </script>
 
-<style scoped>
-.mt-4r {
-  margin-top: 4rem !important;
-}
-.product-testimonials {
-  display: flex;
-  padding-top: 4rem;
-  box-sizing: border-box;
-  min-height: 400px;
-}
-
-.carousel-indicators {
-  top: 350px;
-}
-
-.testimonials {
-  margin-top: 3em;
-  background: var(--clr-primary);
-  padding: 8em 3em;
-  z-index: 0;
-  color: white;
-}
-
-.testimonials-heading-bg {
-  left: -130%;
-  z-index: -1;
-}
-
-.testimonials__heading {
-  margin-right: 5em;
-}
-
-.testimonial__content {
-  font-size: 24px;
-  letter-spacing: 0.4px;
-  line-height: 32px;
-}
-
-.testimonial__person {
-  margin-top: 1.6em;
+<style>
+.testimonials-section {
+  font-family: 'Inter';
+  font-style: normal;
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 1.6fr;
   align-items: center;
-  justify-content: space-between;
+  gap: 150px;
+  padding-top: 120px;
+  padding-bottom: 120px;
 }
-
-.testimonial__person--name {
-  font-size: var(--ft-big-body);
-  font-weight: 900;
+.testimonials-heading {
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 56px;
+  margin-bottom: 64px;
+  color: #211447;
 }
-
-.testimonial__person--post {
-  font-size: var(--ft-df-body);
-  letter-spacing: 2%;
-  color: var(--clr-primary-light);
-}
-
-.quotes {
+.quote-illustraion {
   position: absolute;
-  top: 5%;
-  right: -20%;
-  z-index: -1;
+  top: 0;
+  right: 46%;
 }
-
-@media (max-width: 1400px) {
-  .col-lg-7 {
-    width: 55%;
-  }
-  .testimonials-heading-bg {
-    left: -165%;
-  }
+.user {
+  padding: 20px 28px;
+  border-radius: 8px;
+  margin: 8px 0px;
+  gap: 32px;
+  font-weight: 500;
+  cursor: pointer;
+  user-select: none;
 }
-@media (max-width: 1200px) {
-  .testimonials-heading-bg {
-    left: -207%;
-  }
-  .product-testimonials {
-    padding-top: 3rem;
-  }
+.user.active {
+  background: #ffffff;
+  box-shadow: 0px 17px 80px rgba(0, 0, 0, 0.03),
+    0px 11.0185px 46.8519px rgba(0, 0, 0, 0.0227778),
+    0px 6.54815px 25.4815px rgba(0, 0, 0, 0.0182222),
+    0px 3.4px 13px rgba(0, 0, 0, 0.015),
+    0px 1.38519px 6.51852px rgba(0, 0, 0, 0.0117778),
+    0px 0.314815px 3.14815px rgba(0, 0, 0, 0.00722222);
 }
-
-@media (max-width: 1080px) {
-  .testimonials-heading-bg {
-    left: -225%;
-  }
+.user .name {
+  font-size: 18px;
+  line-height: 28px;
+  color: #211447;
 }
-
-@media (max-width: 1080px) {
-  .quotes {
-    right: -10%;
-    top: 25%;
-  }
+.user .designation {
+  font-size: 16px;
+  line-height: 24px;
+  color: #828282;
 }
-
-@media (max-width: 992px) {
-  .col-lg-7 {
-    width: 100%;
-  }
-}
-
-@media (max-width: 768px) {
-  .product-testimonials {
-    padding-top: 2rem;
-  }
-}
-
-@media (max-width: 580px) {
-  .testimonials {
-    padding: 5em 0.75em 0em 0.75em;
-  }
-  .product-testimonials {
-    padding-top: 1em;
-  }
-  .mt-4r {
-    margin-top: 1rem !important;
-  }
-  .testimonial__person {
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: space-between;
-  }
-
-  .testimonials-heading-bg {
-    display: none;
-  }
-
-  .section__subtitle {
-    font-size: 14px;
-    line-height: 21px;
-  }
-
-  .quotes {
-    right: -15%;
-    top: 30%;
-  }
-
-  .testimonial__content {
-    font-size: 18px;
-    line-height: 28px;
-  }
-
-  .company__logo {
-    margin-top: 1rem;
-    height: 48px;
-  }
-
-  .product-testimonials {
-    min-height: 500px;
-  }
-
-  .carousel-indicators {
-    top: 420px;
-  }
+.testimonial-content-wrapper {
+  font-size: 24px;
+  line-height: 38px;
+  color: #333333;
+  z-index: 2;
 }
 </style>
