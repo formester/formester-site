afc3ba452c7d9deeef92007e4fa3d8d7e8735d96
Ankit Singhaniya <ankit.singhaniyaz@gmail.com>
2023-04-24 19:26:54 +0530

    add what not having spam protextion can do section

--- components/SpamPreventionTipsSection.vue
+++ components/SpamPreventionTipsSection.vue
@@ -20,6 +20,28 @@
             </div>
         </div>
     </div>
+
+    <div class="row mt-4">
+        <div class="heading d-flex flex-column align-items-center text-center">
+            <h2 class="section__heading">What Not Having Spam Protection can do?</h2>
+        </div>
+
+        <div class="templates mt-md-5">
+            <div v-for="(noSpam, idx) in notHaving" :key="idx">
+                    <div class="card p-3 d-flex align-items-center justify-items-center text-center align-items-md-start text-md-start">
+                        <img
+                            :src="require(`../assets/images/icons/${noSpam.imageSrc}`)"
+                            class="img-fluid no__sapm-image"
+                            style="width: 120px;"
+                            :alt="noSpam.altText"
+                        />
+                        <h4 class="sub__section-heading mt-4">{{ noSpam.title }}</h4>
+                        <p  class="subheading__text">{{ noSpam.description }}</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+
   </div>
 </template>
 
@@ -52,6 +74,32 @@ export default {
           imageSrc: 'firewall.jpg',
           altText: 'use web application firewall to block spam '
         },
+      ],
+      notHaving: [
+        {
+          title: 'Inaccurate data & analytics',
+          description: 'Traffic gained, including that which came through spam bots, evidently makes the data inaccurate, leading to skewed performance analytics.',
+          imageSrc: 'idea.jpg',
+          altText: 'prevent spam using google reCaptcha'
+        },
+        {
+          title: 'Inefficient use of platforms',
+          description: 'If your business website hosts classified ads, listings, employment openings, etc., chances are highly likely, that you receive fake leads on your forms.',
+          imageSrc: 'inefficient.jpg',
+          altText: 'have double opt-in to confirm user'
+        },
+        {
+          title: 'Lowered User Experience & Engagement',
+          description: "Spam bots are a hindrance to organic conversations and engagements and annoy visitors with gratuitous comments, messages and advertisements.",
+          imageSrc: 'speed.jpg',
+          altText: 'have a secure web hosting'
+        },
+        {
+          title: 'Web Application Firewall',
+          description: 'Use a WAF to identify and block malicious and spam bot-rendered HTTP/S traffic to your application.',
+          imageSrc: 'experience.jpg',
+          altText: 'use web application firewall to block spam '
+        },
       ]
     }
   }
@@ -78,6 +126,9 @@ export default {
     border: none;
 }
 
+.no__sapm-image {
+  margin-left: -15px;
+}
 @media (max-width: 991px) {
   .templates {
     grid-template-columns: 1fr 1fr;
@@ -87,6 +138,9 @@ export default {
 @media only screen and (max-width: 765px) {
   .templates {
     grid-template-columns: 1fr;
+  }
+  .no__sapm-image {
+    margin-left: 0px;
   }
 }
 </style>
