7e0cf6ced4fba68b6b2e0fb2ca3a242a4fa9a350
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-07-10 14:07:15 +0530

    schema not appending to the jsonld fixed

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -283,7 +283,8 @@ export default {
       imagesArray.push(...this.article.metaImages)
     }
 
-    const jsonData = {
+    const jsonData = [
+      {
       '@context': 'https://schema.org',
       '@type': 'BlogPosting',
       mainEntityOfPage: {
@@ -307,13 +308,14 @@ export default {
         },
       },
       datePublished: this.article.createdAt,
-    }
+      },
+    ]
 
      // Append schema if available
      if (this.article.schema) {
       try {
         this.article.schema.forEach((s) => {
-          parsedSchema = JSON.parse(s)
+          let parsedSchema = JSON.parse(s.type)
           if (typeof parsedSchema == 'object') {
             jsonData.push(parsedSchema)
           }
