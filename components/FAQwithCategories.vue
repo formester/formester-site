1058bdf49ac699fd742434daa48fed5774f51522
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-08-03 09:30:39 +0530

    fixed minor styling issues in faq with categories

--- components/FAQwithCategories.vue
+++ components/FAQwithCategories.vue
@@ -2,7 +2,7 @@
   <section class="container py-2 py-lg-5">
     <h2 class="text-center section__heading">Frequently Asked Questions</h2>
     <div class="row">
-      <div v-for="(category, idx) in categories" :key="idx" class="col-6 my-5">
+      <div v-for="(category, idx) in categories" :key="idx" class="col-md-6 my-5">
         <h3 class="faq__heading">{{ category.title }}</h3>
         <div class="accordion accordion-flush" id="accordionFaqs">
           <div
@@ -19,7 +19,7 @@
                 aria-expanded="false"
                 :aria-controls="`collapse-${idx}-${index}`"
               >
-                {{ faq.question }}
+                <span class="me-2">{{ faq.question }}</span>
               </button>
             </h2>
             <div
@@ -58,6 +58,7 @@ export default {
 .accordion-button {
   font-size: 16px;
   font-weight: 500;
+  line-height: 24px;
   color: rgb(0, 0, 0);
 }
 .accordion-item {
