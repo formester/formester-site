6b6b5b980155bd874a9c31189c694893ac7444ef
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-09-10 12:02:08 +0530

    popular use case component for competitor pages

---
+++ components/competitors/PopularUseCase.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container py-5">
+    <div class="heading d-flex flex-column align-items-center text-center">
+      <h2 class="section__heading">{{ featureData.heading }}</h2>
+    </div>
+
+    <div class="row mt-4">
+      <div
+        v-for="feature in featureData.features"
+        :key="feature.title"
+        class="col-lg-3 col-md-6"
+      >
+        <div class="p-3 d-flex flex-column align-items-center">
+          <nuxt-img
+            :src="feature.img"
+            class="img-fluid"
+            :alt="feature.altText"
+            loading="lazy"
+          />
+          <div class="text-start">
+            <h4 class="sub__section-heading mt-4">{{ feature.title }}</h4>
+            <p class="subheading__text">{{ feature.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    featureData: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.sub__section-heading {
+  font-size: 22px;
+  font-weight: 600;
+}
+
+.subheading__text {
+  font-size: 16px;
+  line-height: 24px;
+  color: #525252;
+}
+</style>
