e92780fde0a66e2136d29253780dab64d7573c16
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-09-10 12:01:53 +0530

    stand-out component for competitor pages

---
+++ components/competitors/StandOutFeatureSection.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container py-5">
+    <div class="heading d-flex flex-column align-items-center text-center">
+      <h2 class="section__heading">
+        {{ featureData.heading }}
+      </h2>
+    </div>
+
+    <div class="row">
+      <div
+        v-for="feature in featureData.features"
+        :key="feature.title"
+        class="col-lg-3 col-md-4 col-sm-6 mt-5"
+      >
+        <div
+          class="border-sm-none border-start ps-3 d-flex flex-column align-items-start"
+        >
+          <nuxt-img
+            :src="feature.img"
+            class="img-fluid"
+            :alt="feature.altText"
+            loading="lazy"
+          />
+          <h4 class="sub__section-heading mt-4">{{ feature.title }}</h4>
+          <p class="subheading__text">{{ feature.description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    featureData: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.sub__section-heading {
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 26px;
+}
+
+.subheading__text {
+  font-size: 14px;
+  line-height: 22px;
+}
+</style>
