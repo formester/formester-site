acfb654668eff4316ba1ce0da7e2ad6ef048ab53
“DeeptanshuPrasad” <deeptanshu@acornglobus.com>
2023-09-30 18:11:25 +0530

    Fixed Images On Generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,22 @@ ssr: true,
 
 nitro: {
   prerender: {
-    ignore: ['/blog'],
+    crawlLinks: true,
+    failOnError: false,
+    autoSubfolderIndex: true,
   },
+  routeRules: {
+    '/blog/**': { swr: true },
+    '/blog/**': { swr: 600 },
+    '/blog/**': { static: true },
+    '/blog/**': { cache: { /* cache options*/ } },
+    '/assets/**': { headers: { 'cache-control': 's-maxage=0' } },
+    '/api/v1/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+    '/old-page': { redirect: '/new-page' }, // uses status code 307 (Temporary Redirect)
+    '/old-page2': { redirect: { to:'/new-page2', statusCode: 301 } },
+    '/proxy/example': { proxy: 'https://example.com' },
+    '/proxy/**': { proxy: '/api/**' },
+  }
 },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
