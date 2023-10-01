6e025dc728341109d017ccf0c50a235eb54d1bde
“DeeptanshuPrasad” <deeptanshu@acornglobus.com>
2023-10-01 13:26:08 +0530

    Base URL Fix #5

--- nuxt.config.js
+++ nuxt.config.js
@@ -144,7 +144,7 @@ app: {
     dir: 'assets/images',
     provider: 'netlify',
     netlify: {
-      baseURL: `${process.env.IMAGES_URL}/assets/images`
+      baseURL: process.env.IMAGES_URL
     }
   },
   // Enviornment variable for the base url of the app
