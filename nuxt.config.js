f8468a3bcb39a76504a167d6177988efdcbef8a0
“DeeptanshuPrasad” <deeptanshu@acornglobus.com>
2023-10-01 13:14:30 +0530

    Fix base url 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -144,7 +144,7 @@ app: {
     dir: '~assets/images',
     provider: 'netlify',
     netlify: {
-      baseURl: `${process.env.IMAGES_URL}/assets/images`
+      baseURl: `${process.env.IMAGES_URL}/assets/images` || 'https://staging--formester-staging.netlify.app/assets/images'
     }
   },
   // Enviornment variable for the base url of the app
