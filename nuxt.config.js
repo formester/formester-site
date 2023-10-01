d3221399ec9e4ea86f79ecfce396ca48526cc308
“DeeptanshuPrasad” <deeptanshu@acornglobus.com>
2023-10-01 15:12:43 +0530

    fix size #5

--- nuxt.config.js
+++ nuxt.config.js
@@ -135,15 +135,7 @@ app: {
 
   // Nuxt Image
   image: {
-    screens: {
-      'xs': 320,
-      'sm': 640,
-      'md': 768,
-      'lg': 1024,
-      'xl': 1280,
-      'xxl': 1536,
-      '2xl': 1536
-    },
+    sizes: '100vw',
     dir: 'assets/images',
     provider: 'netlify',
     netlify: {
