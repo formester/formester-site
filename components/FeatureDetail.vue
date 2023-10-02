e149413c23b27fe9cef401c7385dddc35422e850
“DeeptanshuPrasad” <deeptanshu@acornglobus.com>
2023-10-02 14:50:34 +0530

    Attempt To Fix Image Size #9

--- components/FeatureDetail.vue
+++ components/FeatureDetail.vue
@@ -5,7 +5,7 @@
       class="img-fluid"
       :alt="feature.alt"
       loading="lazy"
-      sizes="70vw"
+      sizes="30vw"
     />
     <h3 class="feature__heading">{{ feature.title }}</h3>
     <p>
