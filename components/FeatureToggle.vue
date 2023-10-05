34c492e60838343a0cc010cceb834f66aa5b1b0d
siddharth0807 <siddharthkumar0807@gmail.com>
2023-10-06 00:55:43 +0530

    deleted the featurelist and advance-feature and made it into single component

--- components/FeatureToggle.vue
+++ components/FeatureToggle.vue
@@ -1,51 +1,60 @@
 <template>
-  <div>
-    <div
-      v-for="(group, groupIndex) in computedFeatures"
-      :key="groupIndex"
-      class="d-flex align-items-center justify-content-center toggler__container mx-auto mt-4"
-    >
-      <span
-        v-for="(feature, index) in group"
-        :key="index"
-        class="toggler__button"
-        :class="activeIndex === groupIndex * 5 + index ? 'active' : ''"
-        @click="setActive(groupIndex * 5 + index)"
-        v-html="feature.heading"
-      ></span>
-    </div>
-    <div
-      class="d-flex align-items-center toggler__container mobile mx-auto mt-4"
-    >
-      <span
-        v-for="(feature, index) in features"
-        :key="index"
-        class="toggler__button"
-        :class="activeIndex === index ? 'active' : ''"
-        @click="setActive(index)"
-        v-html="feature.heading"
-      ></span>
-    </div>
-    <div class="row py-3">
-      <div
-        class="col-lg-7 d-flex flex-column justify-content-center align-items-lg-start align-items-center text-center text-lg-start mt-5"
-      >
-        <h3
-          class="feature__heading"
-          v-html="features[activeIndex].heading"
-        ></h3>
-        <p class="feature__description mt-3">
-          {{ features[activeIndex].content }}
-        </p>
-      </div>
-      <div
-        class="col-lg-5 d-flex align-items-center justify-content-center mt-lg-0 mt-5"
-      >
-        <img
-          :src="require(`@/assets/images/${features[activeIndex].imgName}.svg`)"
-          :alt="features[activeIndex].alt"
-          class="img-fluid feature__img"
-        />
+  <div class="feature__hero">
+    <div class="container py-5 my-3">
+      <h2 class="section__heading text-center">
+        {{ sectionHeading }}
+      </h2>
+      <div>
+        <div
+          v-for="(group, groupIndex) in computedFeatures"
+          :key="groupIndex"
+          class="d-flex align-items-center justify-content-center toggler__container mx-auto mt-4"
+        >
+          <span
+            v-for="(feature, index) in group"
+            :key="index"
+            class="toggler__button"
+            :class="activeIndex === groupIndex * 5 + index ? 'active' : ''"
+            @click="setActive(groupIndex * 5 + index)"
+            v-html="feature.heading"
+          ></span>
+        </div>
+        <div
+          class="d-flex align-items-center toggler__container mobile mx-auto mt-4"
+        >
+          <span
+            v-for="(feature, index) in features"
+            :key="index"
+            class="toggler__button"
+            :class="activeIndex === index ? 'active' : ''"
+            @click="setActive(index)"
+            v-html="feature.heading"
+          ></span>
+        </div>
+        <div class="row py-3">
+          <div
+            class="col-lg-7 d-flex flex-column justify-content-center align-items-lg-start align-items-center text-center text-lg-start mt-5"
+          >
+            <h3
+              class="feature__heading"
+              v-html="features[activeIndex].heading"
+            ></h3>
+            <p class="feature__description mt-3">
+              {{ features[activeIndex].content }}
+            </p>
+          </div>
+          <div
+            class="col-lg-5 d-flex align-items-center justify-content-center mt-lg-0 mt-5"
+          >
+            <img
+              :src="
+                require(`@/assets/images/${features[activeIndex].imgName}.svg`)
+              "
+              :alt="features[activeIndex].alt"
+              class="img-fluid feature__img"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -53,7 +62,7 @@
 
 <script>
 export default {
-  props: ['features'],
+  props: ['features', 'sectionHeading'],
   data() {
     return {
       activeIndex: 0,
@@ -105,12 +114,6 @@ export default {
   background: white;
   box-shadow: 0px -2px 2px 0px rgba(0, 0, 0, 0.05) inset;
 }
-
-/* .section__heading,
-  .section__description,
-  .section__img {
-    transition: opacity 300ms ease-in-out;
-  } */
 
 .feature__heading {
   font-size: 32px;
