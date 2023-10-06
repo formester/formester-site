973b7131edb5d19ee26efede2d8300827268560d
“DeeptanshuPrasad” <deeptanshu@acornglobus.com>
2023-10-06 17:25:26 +0530

    Revert "Fix JsonLd WebPage Schema For Template Page"
    This reverts commit 572d33929fd1cc367c5ecd66af4b58da9ea178d4, reversing
    changes made to b7ea421fd1dc550da6c0b4f379105b4b69be2df9.

--- pages/templates/[...slug].vue
+++ pages/templates/[...slug].vue
@@ -83,13 +83,8 @@ const closePreviewModal = () => {
 }
 
 const meta = computed(() => {
-  const {
-    name,
-    description,
-    metaTitle,
-    metaDescription,
-    previewImageUrl,
-  } = template.value || {}
+  const { name, description, metaTitle, metaDescription, previewImageUrl, keywords } =
+    template.value || {}
   const metaData = {
     type: 'website',
     url: template.value ? `https://formester.com/templates/${route.slug}/` : '',
@@ -137,42 +132,13 @@ const faqsSchema = computed(() => {
 })
 
 useJsonld(() => {
-  const { name, description, previewImageUrl, category } = this.template || {}
-  return [
-  {
-      '@context': 'https://schema.org',
-      '@type': 'WebPage',
-      name: name,
-      description: description,
-      image: previewImageUrl,
-      url: `https://formester.com/templates/${route.slug}/`,
-      mainEntity: {
-        '@type': 'CreativeWork',
-        name: name,
-        description: description,
-        image: previewImageUrl,
-        url: `https://formester.com/templates/${route.slug}/`,
-        author: {
-          '@type': 'Organization',
-          name: 'Formester',
-          url: 'https://formester.com/',
-        },
-      },
-      isPartOf: {
-        '@type': 'CollectionPage',
-        name: category?.name,
-        url: `https://formester.com/templates/categories/${category?.slug}/`,
-        description:
-          'Browse our collection of Research Form templates for free.',
-      },
-    },
-    {
-      '@context': 'https://schema.org',
-      '@type': 'FAQPage',
-      mainEntity: faqsSchema.value,
-    }
-  ]
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: faqsSchema.value,
+  }
 })
+
 </script>
 
 <style scoped>
