12cf54f01b1a7526cd566a9bcfb16fdd88925905
MOHAMMAD HANZALA KHAN <hanzalakhan409@gmail.com>
2023-10-09 13:41:44 +0530

    edit content as per attached docs

--- components/home/HowItWorks.vue
+++ components/home/HowItWorks.vue
@@ -7,13 +7,14 @@
                 <li data-bs-target="#stepsSlider" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Step 1"></li>
                 <li data-bs-target="#stepsSlider" data-bs-slide-to="1" aria-label="Step 2"></li>
                 <li data-bs-target="#stepsSlider" data-bs-slide-to="2" aria-label="Step 3"></li>
+                <li data-bs-target="#stepsSlider" data-bs-slide-to="3" aria-label="Step 4"></li>
             </ol>
             <div class="carousel-inner px-3 py-5">
                 <div class="carousel-item active">
                     <div class="d-flex flex-column-reverse align-items-center flex-lg-row ">
                         <div class="col-lg-6 me-lg-5 mt-5  ">
-                            <h3 class="step__section-heading"><span class="hglt">Create</span> a form</h3>
-                            <span class="">Use Formester backend link to power any existing HTML form on the web or create a sharable hosted form using our form builder tool.</span>
+                            <h3 class="step__section-heading">Create</h3>
+                            <span class="">Start with a template or drag and drop different question types to fit your form needs.</span>
                         </div>
                         <div class="col-lg-5 m-auto">
                             <nuxt-img src="/home/how-it-works/create-a-form.svg" class="w-lg-40 d-block img-fluid my-auto" alt="build your form easy with formester form builder" loading="lazy"/>
@@ -23,8 +24,8 @@
                 <div class="carousel-item">
                     <div class="d-flex flex-column-reverse flex-lg-row align-items-center flex-lg-row">
                         <div class="col-lg-6 ms-lg-5 mt-5  ">
-                            <h3 class="step__section-heading"><span class="hglt">Spam</span> filtering</h3>
-                            <span class="">Easily filter out spam using ReCaptcha. It takes a few clicks to configure while allowing you to control all settings when you have to. No expert knowledge is required.</span>
+                            <h3 class="step__section-heading">Customize</h3>
+                            <span class="">Add your logo and branding for a more personalized look. Send an automated response for a personalized experience.</span>
                         </div>
                         <div class="col-lg-5 m-auto">
                             <nuxt-img src="/home/how-it-works/spam-filtering.svg" class="w-lg-40 d-block img-fluid my-auto" alt="share your form" loading="lazy"/>
@@ -34,8 +35,19 @@
                 <div class="carousel-item">
                     <div class="d-flex flex-column-reverse align-items-center flex-lg-row ">
                         <div class="col-lg-6 me-lg-5 mt-5 ">
-                            <h3 class="step__section-heading"><span class="hglt">Automatic</span> response</h3>
-                            <span class="">Replying to submissions is automatic. You can create and configure emails to go to your users on submission. We also notify you via email, Slack or any other way you like.</span>
+                            <h3 class="step__section-heading">Share</h3>
+                            <span class="">Send your form to real people and watch your form submission roll in.</span>
+                        </div>
+                        <div class="col-lg-5 m-auto">
+                            <nuxt-img src="/home/how-it-works/automatic-response.svg" class="w-lg-40 d-block img-fluid my-auto" alt="Third slide" loading="lazy"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex flex-column-reverse align-items-center flex-lg-row ">
+                        <div class="col-lg-6 me-lg-5 mt-5 ">
+                            <h3 class="step__section-heading">Analyze</h3>
+                            <span class="">Turn form insights into actions with built-in analytics.</span>
                         </div>
                         <div class="col-lg-5 m-auto">
                             <nuxt-img src="/home/how-it-works/automatic-response.svg" class="w-lg-40 d-block img-fluid my-auto" alt="Third slide" loading="lazy"/>
