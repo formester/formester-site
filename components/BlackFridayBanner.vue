c403b9b41cf9229de8248866aef84739aaf734cf
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2023-11-18 09:03:11 +0530

    black friday banner

---
+++ components/BlackFridayBanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="d-flex align-items-center justify-content-center gap-8 bg__primary py-2"
+  >
+    <p class="banner__text">Don’t miss out 40% off Black Friday Deal</p>
+    <nuxt-img src="/confetti.svg" />
+    <nuxt-img src="/chevron-right.svg" />
+  </div>
+</template>
+
+<style scoped>
+.bg__primary {
+  background-color: var(--clr-primary);
+  color: white;
+}
+
+.banner__text {
+  font-size: 18px;
+  margin: 0;
+}
+
+.gap-8 {
+  gap: 8px;
+}
+</style>
