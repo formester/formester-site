06a7c55afba251854277b08fa7e9ebd00fda1f33
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2024-06-06 06:46:04 +0530

    feat: comparison card

---
+++ components/comparision/ComparisonCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="comparison__card">
+    <div class="comparison__logo-wrapper text-center">
+      <nuxt-img
+        v-if="selectedOption"
+        :src="formBuilderLogoSrc[selectedOption]"
+      />
+      <nuxt-img v-else src="/form-building-platform/plus.svg" />
+    </div>
+    <div class="comparison__dropdown-wrapper">
+      <select class="form-select" v-model="selectedOption">
+        <option
+          v-for="option in options"
+          :key="option"
+          :selected="option === selectedOption"
+        >
+          {{ option }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    defaultSelectedOption: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      selectedOption: this.defaultSelectedOption,
+      formBuilderLogoSrc: {
+        Formester: '/logo.svg',
+        Typeform: '/form-building-platforms/typeform.svg',
+        Jotform: '/form-building-platforms/jotform.svg',
+        Fillout: '/form-building-platforms/fillout.svg',
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+.comparison__card {
+  border-radius: 4px;
+  border: 1px solid #eaecf0;
+  box-shadow: var(--box-shadow-lg);
+}
+
+.comparison__logo-wrapper {
+  padding: 80px 24px 40px;
+}
+
+.comparison__dropdown-wrapper {
+  border-top: 1px solid #eaecf0;
+  padding: 40px 24px 72px;
+}
+</style>
--- pages/comparison-tool.vue
+++ pages/comparison-tool.vue
@@ -1,153 +1,48 @@
 <template>
   <div>
     <ComparisonToolHero />
+    <div class="container comparison__card-container mx-auto">
+      <ComparisonCard :options="options" default-selected-option="Formester" />
+      <ComparisonCard :options="options" default-selected-option="Typeform" />
+      <ComparisonCard :options="options" default-selected-option="Jotform" />
+      <ComparisonCard :options="options" default-selected-option="Fillout" />
+    </div>
   </div>
 </template>
 
 <script>
 import ComparisonToolHero from '@/components/comparision/ComparisonToolHero.vue'
+import ComparisonCard from '@/components/comparision/ComparisonCard.vue'
 
 export default {
   components: {
     ComparisonToolHero,
+    ComparisonCard,
   },
   data() {
     return {
-      showImage: false,
+      options: ['Formester', 'Typeform', 'Jotform', 'Fillout'],
     }
-  },
-  mounted() {
-    this.showImage = true
   },
 }
 </script>
 
 <style scoped>
-.hero {
-  margin-top: 130px;
-  margin-bottom: 100px;
-  max-width: 1440px;
+.comparison__card-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 24px;
 }
 
-.hero__content {
-  max-width: 820px;
-}
-
-.hero__subtitle {
-  color: var(--clr-primary);
-  font-size: 16px;
-  font-weight: 600;
-  line-height: 24px;
-}
-
-.hero__heading {
-  margin-top: 12px;
-  font-size: 48px;
-  font-weight: 600;
-  line-height: 60px;
-  color: var(--clr-text-primary);
-}
-
-.hero__description {
-  color: var(--clr-text-secondary);
-  font-size: 20px;
-  line-height: 30px;
-}
-
-.hero-image {
-  z-index: -1;
-}
-
-.hero-image.left {
-  top: 40%;
-  transform: translateY(-50%);
-  left: 5%;
-  opacity: 1;
-}
-
-.hero-image.right {
-  top: 40%;
-  transform: translateY(-50%);
-  right: 5%;
-  opacity: 1;
-}
-
-.formester-logo {
-  height: 40px;
-}
-
-/* animation */
-.slide-in-left-enter-active {
-  animation: slideInFadeInLeft 0.5s ease-out forwards;
-}
-.slide-in-left-enter {
-  transform: translateX(-100%) translateY(-50%);
-  opacity: 0;
-}
-
-@keyframes slideInFadeInLeft {
-  from {
-    transform: translateX(-100%) translateY(-50%);
-    opacity: 0;
-  }
-  to {
-    transform: translateX(0) translateY(-50%);
-    opacity: 1;
+@media screen and (max-width: 1200px) {
+  .comparison__card-container {
+    grid-template-columns: repeat(2, 1fr);
   }
 }
 
-.slide-in-right-enter-active {
-  animation: slideInFadeInRight 0.5s ease-out forwards;
-}
-.slide-in-right-enter {
-  transform: translateX(100%) translateY(-50%);
-  opacity: 0;
-}
-
-@keyframes slideInFadeInRight {
-  from {
-    transform: translateX(100%) translateY(-50%);
-    opacity: 0;
-  }
-  to {
-    transform: translateX(0) translateY(-50%);
-    opacity: 1;
-  }
-}
-
-@media screen and (max-width: 1100px) {
-  .hero__content {
-    max-width: 524px;
-  }
-}
-
-@media screen and (max-width: 900px) {
-  .hero-image.left {
-    left: 0%;
-  }
-
-  .hero-image.right {
-    right: 0%;
-  }
-}
 @media screen and (max-width: 768px) {
-  .hero-image {
-    display: none;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .hero__heading {
-    font-size: 36px;
-    line-height: 52px;
-  }
-
-  .formester-logo {
-    height: 36px;
-  }
-
-  .wordpress-logo {
-    height: 44px;
+  .comparison__card-container {
+    grid-template-columns: 1fr;
   }
 }
 </style>
