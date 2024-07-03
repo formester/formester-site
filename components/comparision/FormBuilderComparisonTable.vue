283d8bc5d64e14050c5e3d9821af2245f0aeeb1f
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2024-07-03 23:41:23 +0530

    fix: accordion arrow color to default on hover for comparison tool

--- components/comparision/FormBuilderComparisonTable.vue
+++ components/comparision/FormBuilderComparisonTable.vue
@@ -365,6 +365,10 @@ export default {
   box-shadow: none;
 }
 
+.accordion-button:after {
+  background-image: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23212529'><path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/></svg>") !important;
+}
+
 /* === mobile === */
 .mobile__formbuilder-details-wrapper .formbuilder__select-plan {
   margin: 12px auto 20px;
