e86446ba9587964da91e9cfc8970d84270871c9e
Aniket <aniketsatapathy193@gmail.com>
2024-07-08 09:48:14 +0530

    refactor: Update Templates.vue to use v-html for activeCategory description

--- components/template/Templates.vue
+++ components/template/Templates.vue
@@ -27,9 +27,10 @@
           class="my-2"
           :class="{ 'd-flex align-items-center': !showFullDescription }"
         >
-          <p class="content-description mt-0 mb-1">
-            {{ activeCategory.description }}
-          </p>
+          <p
+            class="content-description mt-0 mb-0"
+            v-html="activeCategory.description"
+          ></p>
           <button
             class="content-description-handle-button text-nowrap"
             @click="toggleDescription"
