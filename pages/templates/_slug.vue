0856494e3ca76eff110ca8349c71305a7ebf5e11
Anirudh Tantry <anirudhtan3@gmail.com>
2024-07-19 18:51:18 +0530

    multi paragraph description for template

--- pages/templates/_slug.vue
+++ pages/templates/_slug.vue
@@ -8,9 +8,12 @@
         class="d-flex flex-column justify-content-center align-items-lg-start align-items-center text-center text-lg-start mt-xl-0 mt-md-5 hero__info_container"
       >
         <h1 class="section__heading">{{ template.name }}</h1>
-        <p class="hero__subheading mt-3">
-          {{ template.description }}
+
+        <!-- New paragraph for each line break -->
+        <p v-for="(line, index) in template.description" :key="index" class="hero__subheading mt-3">
+          {{ line }}
         </p>
+        
         <div class="btns-container d-flex align-items-center mt-2">
           <button class="btn btn-use_template" @click="redirectTo">
             Use Template
