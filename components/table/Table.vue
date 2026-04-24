1be83cfa1979c1ef331327b24005a3eea72baa9e
Mohit <mohitbhandari852@gmail.com>
2026-04-24 14:04:00 +0530

    Added highlight column to table components

--- components/table/Table.vue
+++ components/table/Table.vue
@@ -1,48 +1,56 @@
 <template>
-    <div class="container py-5">
-        <div class="row justify-content-center align-items-center text-center ">
-          <SectionTitle :heading="title" />
-            <table class="tablestyle">
-                <thead>
-                    <TableRow :rowData="rows[0].rowData" :rowType="rows[0].rowType" />
-                </thead>
-                <tbody>
-                    <TableRow
-          v-for="(row, index) in rows.slice(1)"
-          :key="index"
-          :rowData="row.rowData"
-          :rowType="row.rowType"
-        />
-      </tbody>
-    </table>
-        </div>
+  <div class="container py-5">
+    <div class="row justify-content-center align-items-center text-center">
+      <SectionTitle :heading="title" />
+      <table class="tablestyle">
+        <thead>
+          <TableRow
+            :rowData="rows[0].rowData"
+            :rowType="rows[0].rowType"
+            :highlightColumn="highlightColumn"
+          />
+        </thead>
+        <tbody>
+          <TableRow
+            v-for="(row, index) in rows.slice(1)"
+            :key="index"
+            :rowData="row.rowData"
+            :rowType="row.rowType"
+            :highlightColumn="highlightColumn"
+          />
+        </tbody>
+      </table>
     </div>
-  </template>
-  
-  <script>
-  import TableRow from './TableRow.vue';
-  
-  export default {
-    props: {
-      title: {
-        type: Array,
-        required: true,
-      },
-      rows: Array,
-    },
-    components: {
-      TableRow
-    },
-  };
-  </script>
-  
-  <style scoped>
+  </div>
+</template>
 
+<script>
+import TableRow from './TableRow.vue'
+
+export default {
+  props: {
+    title: {
+      type: Array,
+      required: true,
+    },
+    rows: Array,
+    highlightColumn: {
+      type: Number,
+      default: null,
+    },
+  },
+  components: {
+    TableRow,
+  },
+}
+</script>
+
+<style scoped>
 .tablestyle {
   margin-top: 32px;
 }
 
- table {
+table {
   width: 100%;
   max-width: 980px;
   border-spacing: 0;
@@ -55,34 +63,34 @@
   border-radius: 12px;
   overflow: hidden;
 }
-  
-  th, td {
-    padding: 12px 16px;
-    text-align: left;
-  }
-  
-  tbody tr {
-    border-bottom: 1.5px solid #EAECF0;
-  }
-  tbody tr:nth-child(even) {
-    background-color: #f9fafb;
-  }
-  tbody tr:last-child {
-    border-bottom: none;
-  }
-  
-  thead tr {
-    background: #f3f4f6;
-  }
-  
-  tbody td {
-    color: #6b7280;
-  }
 
-  @media (max-width: 575px) {
-    table {
-      width: 95%;
-    }
+th,
+td {
+  padding: 12px 16px;
+  text-align: left;
+}
+
+tbody tr {
+  border-bottom: 1.5px solid #eaecf0;
+}
+tbody tr:nth-child(even) {
+  background-color: #f9fafb;
+}
+tbody tr:last-child {
+  border-bottom: none;
+}
+
+thead tr {
+  background: #f3f4f6;
+}
+
+tbody td {
+  color: #6b7280;
+}
+
+@media (max-width: 575px) {
+  table {
+    width: 95%;
   }
-  </style>
-  
+}
+</style>
