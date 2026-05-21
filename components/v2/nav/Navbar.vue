2efdf1279282b04029004551c5c0c05c99726041
abhay2133 <abhaybishthestudent@gmail.com>
2026-05-21 14:00:20 +0530

    fix: adjust navbar background color and backdrop filter for improved visibility

--- components/v2/nav/Navbar.vue
+++ components/v2/nav/Navbar.vue
@@ -307,8 +307,8 @@ onBeforeUnmount(() => {
 
 <style scoped>
 .navbar {
-  background-color: rgba(255, 255, 255, 0.8);
-  backdrop-filter: blur(20px);
+  background-color: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(8px);
   top: 0;
 }
 
