d843b82b9a97d91019695b69aab092b44ab857cc
anirudh-tantry <anirudhtan3@gmail.com>
2026-06-17 06:35:17 +0000

    Replace inline svg

--- components/v2/nav/IntegrationsDropdown.vue
+++ components/v2/nav/IntegrationsDropdown.vue
@@ -93,9 +93,7 @@
           <div class="intg-footer">
             <NuxtLink to="/integrations/" class="intg-all-link" @click="$emit('dropdown-close')">
               Browse all integrations
-              <svg width="14" height="14" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path d="M3 8h10M9 4l4 4-4 4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
+              <nuxt-img src="/arrow-right.svg" alt="" width="14" height="14" aria-hidden="true" />
             </NuxtLink>
           </div>
         </div>
